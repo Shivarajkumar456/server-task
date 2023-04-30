228c7f67fa57cd4f340adb8e123d83a08d9f7f29
shivarajkumar <shivaraj1654@gmail.com>
2023-04-30 17:24:11 +0530

    task-9 clean-up

--- app.js
+++ app.js
@@ -1,50 +1,9 @@
 const http = require('http');
-const fs=require('fs');
+const file = require('./files');
 
-const server = http.createServer((req, res) => {
-  const url=req.url;
-  const method=req.method;
-  
-  if(url==="/"){
-        fs.readFile("message.txt",{encoding: "utf-8"},(err,data)=>{
-            if(err)
-            {
-                console.log(err)
-            }
-        
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>');
-        res.write('<head> <title> my first page </title></head>');
-        res.write(`<body>${data}</body>`)
-        res.write('<body><form action="/message" method="POST" ><input type="text" name="message"><button type="submit">Send</button></form></body');
-        res.write('<html>')
-        return res.end();
-        })
-    }
-   else if(url==='/message' && method === 'POST'){
-        const body=[];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk)
-            console.log(body);
-        });
-        req.on('end',()=>{
-            const parseBody=Buffer.concat(body).toString();
-            const message=parseBody.split('=')[1];
-            fs.writeFileSync('message.txt',message);
-        })
-        res.statusCode=302;
-        res.setHeader('Location','/');
-        return res.end();
-    }else{
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>NodeJs Page</title><head>');
-    res.write('<body><h1>Hello World!</h1></body>');
-    res.write('</html>');
-    res.end();
-   }
-});
+console.log(file.message)
+
+const server = http.createServer(file.handler);
 
 server.listen(4000, () => {
   console.log('Server running at http://localhost:4000/');
---
+++ files.js
@@ -0,0 +1,59 @@
+const fs=require('fs');
+
+const reqHandler = (req, res) => {
+  const url=req.url;
+  const method=req.method;
+  
+  if(url==="/"){
+        fs.readFile("message.txt",{encoding: "utf-8"},(err,data)=>{
+            if(err)
+            {
+                console.log(err)
+            }
+        
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head> <title> my first page </title></head>');
+        res.write(`<body>${data}</body>`)
+        res.write('<body><form action="/message" method="POST" ><input type="text" name="message"><button type="submit">Send</button></form></body');
+        res.write('<html>')
+        return res.end();
+        })
+    }
+   else if(url==='/message' && method === 'POST'){
+        const body=[];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk)
+            console.log(body);
+        });
+        req.on('end',()=>{
+            const parseBody=Buffer.concat(body).toString();
+            const message=parseBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+        })
+        res.statusCode=302;
+        res.setHeader('Location','/');
+        return res.end();
+    }else{
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>NodeJs Page</title><head>');
+    res.write('<body><h1>Hello World!</h1></body>');
+    res.write('</html>');
+    res.end();
+   }
+};
+
+// module.exports = reqHandler;
+
+// module.exports = {
+//     handler: reqHandler,
+//     message : 'Dummy message'
+// }
+
+// module.exports.handler = reqHandler;
+// module.exports.message = 'Dummy message';
+
+exports.handler = reqHandler;
+exports.message = 'Dummy message';
