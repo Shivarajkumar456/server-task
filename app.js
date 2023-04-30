02aff09f12c635d13c96a3a6ba93b17badd93fbf
shivarajkumar <shivaraj1654@gmail.com>
2023-04-30 14:57:51 +0530

    task 8

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
 const http = require('http');
+const fs=require('fs');
 
 const server = http.createServer((req, res) => {
   const url=req.url;
-  if(url==='/home')
-  {
+  const method=req.method;
+  
+  if(url==="/"){
+        fs.readFile("message.txt",{encoding: "utf-8"},(err,data)=>{
+            if(err)
+            {
+                console.log(err)
+            }
+        
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head> <title> my first page </title></head>');
+        res.write(`<body>${data}</body>`)
+        res.write('<body><form action="/message" method="POST" ><input type="text" name="message"><button type="submit">Send</button></form></body');
+        res.write('<html>')
+        return res.end();
+        })
+    }
+   else if(url==='/message' && method === 'POST'){
+        const body=[];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk)
+            console.log(body);
+        });
+        req.on('end',()=>{
+            const parseBody=Buffer.concat(body).toString();
+            const message=parseBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+        })
+        res.statusCode=302;
+        res.setHeader('Location','/');
+        return res.end();
+    }else{
+    res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
-    res.write('<head> <title> Home Page </title></head>');
-    res.write('<body><h1>Welcome to home page</h1></body');
-    res.write('<html>')
-    return res.end();
-  }
-
-  if(url==='/About')
-  {
-    res.write('<html>');
-    res.write('<head> <title> Abbout Page </title></head>');
-    res.write('<body><h1>Welcome to About page</h1></body');
-    res.write('<html>');
-    return res.end()
-  }
-
-  if(url==='/Node')
-  {
-    res.write('<html>');
-    res.write('<head> <title> Node Page </title></head>');
-    res.write('<body><h1>Welcome to my Node Js project</h1></body');
-    res.write('<html>')
-    return res.end();
-  }
-  if(url==='/Random')
-  {
-    res.write('<html>');
-    res.write('<head> <title> Random Page </title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>Send</button></form></body');
-    res.write('<html>')
-    return res.end();
-  }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>First NodeJs Page</title></head>');
-  res.write('<body><h1>Hello World!</h1></body>');
-  res.write('</html>')
-  res.end();
+    res.write('<head><title>NodeJs Page</title><head>');
+    res.write('<body><h1>Hello World!</h1></body>');
+    res.write('</html>');
+    res.end();
+   }
 });
 
 server.listen(4000, () => {
